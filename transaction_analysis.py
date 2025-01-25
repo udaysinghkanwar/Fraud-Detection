9e2f066cdaadd378733015011a48fd666c18c962
udaysinghkanwar <udaykanwar007@gmail.com>
2025-01-24 19:27:15 -0500

    T-SNE

--- transaction_analysis.py
+++ transaction_analysis.py
@@ -108,3 +108,128 @@ print(new_df['Class'].value_counts()/len(new_df))
 sns.countplot(x='Class', data=new_df, palette=colors)
 plt.title('Equally Distributed Classes', fontsize=14)
 plt.show()
+
+#correlation matrix
+
+                             
+sub_sample_corr= new_df.corr()
+ax= sns.heatmap(sub_sample_corr, cmap="coolwarm_r",  annot_kws={'size':20} )
+ax.set_title("Correlation matrix of the subsample data", fontsize=20)
+plt.show()
+
+
+#showing positive and negative correlated features
+f, negative_axes = plt.subplots(ncols=4, figsize= (25,5))
+
+sns.boxplot(x="Class", y="V17", data=new_df, palette=colors, ax=negative_axes[0])
+negative_axes[0].set_title('V17 vs Class Negative Correlation')
+
+sns.boxplot(x="Class", y="V14", data=new_df, palette=colors, ax=negative_axes[1])
+negative_axes[1].set_title('V14 vs Class Negative Correlation')
+
+
+sns.boxplot(x="Class", y="V12", data=new_df, palette=colors, ax=negative_axes[2])
+negative_axes[2].set_title('V12 vs Class Negative Correlation')
+
+
+sns.boxplot(x="Class", y="V10", data=new_df, palette=colors, ax=negative_axes[3])
+negative_axes[3].set_title('V10 vs Class Negative Correlation')
+
+plt.show()
+
+fig, positive_axes = plt.subplots(ncols=4, figsize=(20,4))
+
+
+sns.boxplot(x="Class", y="V11", data=new_df, palette=colors, ax=positive_axes[0])
+positive_axes[0].set_title('V11 vs Class Positive Correlation')
+
+sns.boxplot(x="Class", y="V4", data=new_df, palette=colors, ax=positive_axes[1])
+positive_axes[1].set_title('V4 vs Class Positive Correlation')
+
+
+sns.boxplot(x="Class", y="V2", data=new_df, palette=colors, ax=positive_axes[2])
+positive_axes[2].set_title('V2 vs Class Positive Correlation')
+
+
+sns.boxplot(x="Class", y="V19", data=new_df, palette=colors, ax=positive_axes[3])
+positive_axes[3].set_title('V19 vs Class Positive Correlation')
+
+plt.show()
+
+# removing outliers from the data
+
+print("Removing Outliers...")
+
+v14_fraud = new_df['V14'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v14_fraud, 25), np.percentile(v14_fraud, 75)
+print('Quartile 25: {} | Quartile 75: {}'.format(q25, q75))
+v14_iqr = q75 - q25
+print('iqr range: {}'.format(v14_iqr))
+
+v14_cut_off = v14_iqr * 1.5
+v14_lower, v14_upper = q25 - v14_cut_off, q75 + v14_cut_off
+print('Cut Off: {}'.format(v14_cut_off))
+print('V14 Lower: {}'.format(v14_lower))
+print('V14 Upper: {}'.format(v14_upper))
+
+outliers = [x for x in v14_fraud if x < v14_lower or x > v14_upper]
+print('Number of Outliers in V14 feature for Fraud Cases: {}'.format(len(outliers)))
+print('All outliers in v14:{}'.format(outliers))
+
+new_df = new_df.drop(new_df[(new_df['V14'] > v14_upper) | (new_df['V14'] < v14_lower)].index)
+print('----' * 44)
+
+# removing v12 outliers
+v12_fraud = new_df['V12'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v12_fraud, 25), np.percentile(v12_fraud, 75)
+v12_iqr = q75 - q25
+
+v12_cut_off = v12_iqr * 1.5
+v12_lower, v12_upper = q25 - v12_cut_off, q75 + v12_cut_off
+print('V12 Lower: {}'.format(v12_lower))
+print('V12 Upper: {}'.format(v12_upper))
+outliers = [x for x in v12_fraud if x < v12_lower or x > v12_upper]
+print('V12 outliers: {}'.format(outliers))
+print('Feature V12 Outliers for Fraud Cases: {}'.format(len(outliers)))
+new_df = new_df.drop(new_df[(new_df['V12'] > v12_upper) | (new_df['V12'] < v12_lower)].index)
+print('Number of Instances after outliers removal: {}'.format(len(new_df)))
+print('----' * 44)
+
+#removing v10 outliers
+
+v10_fraud = new_df['V10'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v10_fraud, 25), np.percentile(v10_fraud, 75)
+v10_iqr = q75 - q25
+
+v10_cut_off = v10_iqr * 1.5
+v10_lower, v10_upper = q25 - v10_cut_off, q75 + v10_cut_off
+print('V10 Lower: {}'.format(v10_lower))
+print('V10 Upper: {}'.format(v10_upper))
+outliers = [x for x in v10_fraud if x < v10_lower or x > v10_upper]
+print('V10 outliers: {}'.format(outliers))
+print('Feature V10 Outliers for Fraud Cases: {}'.format(len(outliers)))
+new_df = new_df.drop(new_df[(new_df['V10'] > v10_upper) | (new_df['V10'] < v10_lower)].index)
+print('Number of Instances after outliers removal: {}'.format(len(new_df)))
+
+X = new_df.drop('Class', axis=1)
+y = new_df['Class']
+
+
+# T-SNE Implementation
+t0 = time.time()
+X_reduced_tsne = TSNE(n_components=2, random_state=42).fit_transform(X.values)
+t1 = time.time()
+print("T-SNE took {:.2} s".format(t1 - t0))
+
+# PCA Implementation
+t0 = time.time()
+X_reduced_pca = PCA(n_components=2, random_state=42).fit_transform(X.values)
+t1 = time.time()
+print("PCA took {:.2} s".format(t1 - t0))
+
+
+# TruncatedSVD
+t0 = time.time()
+X_reduced_svd = TruncatedSVD(n_components=2, algorithm='randomized', random_state=42).fit_transform(X.values)
+t1 = time.time()
+print("Truncated SVD took {:.2} s".format(t1 - t0))
